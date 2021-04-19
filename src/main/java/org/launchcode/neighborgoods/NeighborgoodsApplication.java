3f8e6816df35480ee4cb7c99bda48995363427d2
Cailin <cailintouseull@gmail.com>
2021-04-18 20:41:27 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/neighborgoods/NeighborgoodsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.neighborgoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborgoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighborgoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/neighborgoods/NeighborgoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.neighborgoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborgoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
