56a436017dcb7a82335feee4716538d7b67d784e
Sandy Hadley <sandymariehadley@gmail.com>
2021-04-22 20:42:28 -0500

    Added Hello World an changed dependencies

---
+++ src/main/java/org/launchcode/neighborgoods/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package org.launchcode.neighborgoods.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping(value="")
+    @ResponseBody
+    public String index() {
+        return "Hello, World!";
+    }
+}
