705c0f6d70b9a0a87f91cd191fb5740199b81fd7
Cailin <cailintouseull@gmail.com>
2021-04-27 21:06:53 -0500

    working on browse landing

--- src/main/java/org/launchcode/neighborgoods/controllers/BrowseController.java
+++ src/main/java/org/launchcode/neighborgoods/controllers/BrowseController.java
@@ -1,2 +1,32 @@
-package org.launchcode.neighborgoods.controllers;public class BrowseController {
+package org.launchcode.neighborgoods.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+
+@Controller
+@RequestMapping(value = "browse")
+public class BrowseController {
+
+    static HashMap<String, String> categoryType = new HashMap<>();
+    //static HashMap<String, String> categorySubType = new HashMap<>();
+
+    public BrowseController(){
+        categoryType.put("all", "All");
+        categoryType.put("restaurants", "Restaurants");
+        categoryType.put("retail", "Retail");
+        categoryType.put("home repair", "Home Repair");
+        categoryType.put("salons", "Salons");
+
+        //categorySubType.put(*maybe input from database/file*);
+    }
+
+    public String browse(Model model){
+       // model.addAttribute("rows", categoryType);
+
+        return "browse";
+    }
+
 }
