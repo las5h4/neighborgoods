57ab3ad1fb05e5fbd5a02ccd0a89b3c6c6601ac3
Cailin <cailintouseull@gmail.com>
2021-04-28 13:42:19 -0500

    added 2nd unfinished method

--- src/main/java/org/launchcode/neighborgoods/controllers/BrowseController.java
+++ src/main/java/org/launchcode/neighborgoods/controllers/BrowseController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 public class BrowseController {
 
     static HashMap<String, String> categoryType = new HashMap<>();
-    //static HashMap<String, String> categorySubType = new HashMap<>();
+    static HashMap<String, String> categorySubType = new HashMap<>();
 
     public BrowseController(){
         categoryType.put("all", "All");
@@ -20,13 +20,25 @@ public class BrowseController {
         categoryType.put("home repair", "Home Repair");
         categoryType.put("salons", "Salons");
 
-        //categorySubType.put(*maybe input from database/file*);
+        categorySubType.put("breakfast", "Breakfast");
+        categorySubType.put("coffee", "Coffee/Tea");
+
     }
 
+    @RequestMapping(value="")
     public String browse(Model model){
        // model.addAttribute("rows", categoryType);
+        //fill in once data is figured out
 
         return "browse";
+    }
+
+    @RequestMapping(value="")
+    public String browseByCategorySubType(Model model){
+        //this part will render a different view based on which subcategory the user clicks
+        //this part depends on where data is being pulled from
+
+        return "";
     }
 
 }
