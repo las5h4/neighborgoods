737711cc8f03e282c3892ff17ddf53d7247de482
devojw <devowebjw@gmail.com>
2021-04-29 05:55:24 -0500

    Added Carousel, Updated NavBar

--- src/main/java/org/launchcode/neighborgoods/controllers/HomeController.java
+++ src/main/java/org/launchcode/neighborgoods/controllers/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value="")
 public class HomeController {
     @GetMapping("")
-    public String index(){
+    public String homePage(){
         return "index";
     }
 }
